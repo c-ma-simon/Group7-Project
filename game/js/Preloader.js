93fb0ed8213fca819a82c86f7ab13e6979a80a45
jcoll02Git <78711528+jcoll02Git@users.noreply.github.com>
2021-04-22 12:30:45 -0400

    Update Preloader.js

--- game/js/Preloader.js
+++ game/js/Preloader.js
@@ -25,6 +25,7 @@ GameStates.makePreloader = function( game ) {
             //tilemaps
             game.load.tilemap('testmap0', 'assets/testmap00.json', null, Phaser.Tilemap.TILED_JSON);
             game.load.tilemap('testmap1', 'assets/testmap01.json', null, Phaser.Tilemap.TILED_JSON);
+	    game.load.tilemap('gamemap01', 'assets/gamemap01.json', null, Phaser.Tilemap.TILED_JSON);
             //tilesets
             game.load.image('CSprojecttiles00', 'assets/CSprojecttiles00.png');
             //backgrounds
